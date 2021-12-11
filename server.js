522e8e7b07711c1e8c33fab9b34441418a8b5c93
rryan2 <ruijin@usc.edu>
2021-12-11 14:54:46 -0800

    test

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+app.use(express.static('public'));
+app.use('/zombie', express.static('public'));
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
